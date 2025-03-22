79261b2d53a614365539f1738e64a7b6a02181d1
Vikram <pooniavikram348@gmail.com>
2025-03-22 18:37:48 +0530

    setting vercel

--- tempCodeRunnerFile.js
+++
@@ -1,9 +0,0 @@
-
-// MySQL Database Connection
-const db = mysql.createConnection({
-    host: '127.0.0.1',
-    user: 'root',
-    password: 'Vikram#9660',
-    database: 'school_management',
-    port: 3306 
-});
