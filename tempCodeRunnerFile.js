c52ad7622afb8049fd85c7725a3717d456f5a265
Vikram <pooniavikram348@gmail.com>
2025-03-22 18:35:46 +0530

    Loaded all backend files

---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,9 @@
+
+// MySQL Database Connection
+const db = mysql.createConnection({
+    host: '127.0.0.1',
+    user: 'root',
+    password: 'Vikram#9660',
+    database: 'school_management',
+    port: 3306 
+});
